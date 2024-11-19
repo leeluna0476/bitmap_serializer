7485e380ba9f800b37f49afd95b809877ad7a140
seojilee <seojilee@student.42seoul.kr>
2024-11-19 19:55:41 +0900

    add comments to bmp file header

--- bmp_file_format.hpp
+++ bmp_file_format.hpp
@@ -7,11 +7,11 @@
 #pragma pack(push, 1)
 struct	bmp_file_header
 {
-	uint16_t	type;		// magic number "BM"
-	uint32_t	size;		// bmp file size
-	uint16_t	reserved_1;
-	uint16_t	reserved_2;
-	uint32_t	offbits;
+	uint16_t	type;		// The header field used to identify the BMP and DIB file is 0x42 0x4D in hexadecimal, same as BM in ASCII.
+	uint32_t	size;		// The size of the BMP file in bytes.
+	uint16_t	reserved_1;	// Reserved; actual value depends on the application that creates the image, if created manually can be 0
+	uint16_t	reserved_2;	// Reserved; actual value depends on the application that creates the image, if created manually can be 0
+	uint32_t	offbits;	// The offset, i.e. starting address, of the byte where the bitmap image data (pixel array) can be found.
 };
 #pragma pack(pop)
 
